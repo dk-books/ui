d9494f97503e4bfcda6bfa8c5816962081db8041
wai-lok-cheng <wailok.cheng@uk.dk.com>
2023-03-10 17:40:16 +0000

    update dropdown-template grayscale

--- src/components/atoms/dropdown-template/index.tsx
+++ src/components/atoms/dropdown-template/index.tsx
@@ -13,7 +13,7 @@ const DropdownTemplate: FC<DropdownTemplateProps> = ({
 	<>
 		<div
 			className={`absolute bg-white z-20 sm:-left-5 sm:w-mobile-extra md:w-full sm:rounded-b md:rounded-b-3xl sm:border-b 
-        md:border border-solid border-gray-200 py-5 sm:top-11 md:top-6.25 transform origin-top transition ${display ? 'opacity-100 rotateX-0 visible' : 'invisible opacity-30 rotateX-90'}`}>
+        md:border border-solid border-grayscale-dark py-5 sm:top-11 md:top-6.25 transform origin-top transition ${display ? 'opacity-100 rotateX-0 visible' : 'invisible opacity-30 rotateX-90'}`}>
 			{children}
 		</div>
 	</>
