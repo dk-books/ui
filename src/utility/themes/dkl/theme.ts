a961d1fac0d61feb36ccff605a8304c8e2d7d43c
wai-lok-cheng <wailok.cheng@uk.dk.com>
2023-05-22 12:27:02 +0100

    update blue color

--- src/utility/themes/dkl/theme.ts
+++ src/utility/themes/dkl/theme.ts
@@ -1,14 +1,14 @@
 export const dklBtn: (Record<string, string>) = {
 	button: 'font-medium inline-flex justify-center items-center sm:justify-center sm:w-full rounded-full any-hover:hover:underline active:underline any-hover:hover:shadow-md active:shadow-md transition-all mobile-click-clear;',
-	buttonDark: 'bg-blue-500 text-white any-hover:hover:bg-blue-700 active:bg-blue-700',
-	buttonLight: 'bg-blue-100 text-blue-700 any-hover:hover:bg-blue-300 active:bg-blue-300',
+	dark: 'bg-blue-800 any-hover:hover:bg-blue-900 active:bg-blue-700 text-grayscale-100',
+	light: 'bg-blue-200 any-hover:hover:bg-blue-300 active:bg-blue-400 text-dark-blue-600',
 	buttonWhite: 'bg-white border any-hover:hover:bg-gray-300',
 	pink: 'bg-red-200 text-red-700 any-hover:hover:bg-red-300',
 	red: 'bg-red-600 text-white any-hover:hover:bg-red-700',
 	orange: 'bg-orange-700 any-hover:hover:bg-orange-800 active:bg-orange-600 text-grayscale-100',
 	yellow: 'bg-dark-yellow-700 any-hover:hover:bg-dark-yellow-800 active:bg-dark-yellow-600 text-grayscale-100',
 	green: 'bg-turquoise-700 any-hover:hover:bg-turquoise-900 active:bg-turquoise-600 text-grayscale-100',
-	blue: 'bg-blue-950 text-white any-hover:hover:bg-blue-1050;',
+	blue: 'bg-dkl-blue any-hover:hover:bg-blue-900 active:bg-blue-700 text-grayscale-100',
 	purple: 'bg-purple-800 any-hover:hover:bg-purple-900 active:bg-purple-600 text-grayscale-100',
-	default: 'text-sm bg-blue-500 text-white any-hover:hover:bg-blue-700 active:bg-blue-700',
+	default: 'bg-blue-800 any-hover:hover:bg-blue-900 active:bg-blue-700 text-grayscale-100',
 };
--- src/utility/themes/efe/theme.ts
+++ src/utility/themes/efe/theme.ts
@@ -1,15 +1,15 @@
 export const efeBtn: (Record<string, string>) = {
 	button: 'sm:w-full flex sm:text-14 text-16 xl:text-18 text-white rounded-lg items-center py-3 px-6 text-lg py-3 px-6 mx-auto text-center items-center rounded-lg mx-0 whitespace-nowrap',
-	blue: 'bg-blue-350 hover:bg-blue-300',
+	blue: 'bg-blue-700 hover:bg-dark-blue-700',
 	green: 'bg-green-700 hover:bg-green-900',
 	red: 'bg-red-300 hover:bg-red-550',
 	orange: 'bg-orange-700 hover:bg-orange-900',
 	black: 'bg-black-300 hover:bg-black-500',
 	gray: 'bg-gray-300 hover:bg-black-500',
-	navy: 'bg-blue-350 hover:bg-blue-300',
+	navy: 'bg-blue-700 hover:bg-dark-blue-700',
 	teal: 'bg-turquoise-700 hover:bg-turquoise-900',
 	turquoise: 'bg-turquoise-700 hover:bg-turquoise-900',
-	default: 'bg-blue-350 hover:bg-blue-300',
+	default: 'bg-blue-700 hover:bg-dark-blue-700',
 };
 
 export const fontColor: (Record<string, string>) = {
