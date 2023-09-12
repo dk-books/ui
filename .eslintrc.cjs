ae8240034f64fe707e904c10e39b5a4b64a72509
owestbury <owestbury@gmail.com>
2023-09-12 21:39:09 +0100

    fix: button bug

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,10 @@
 module.exports = {
   root: true,
-  env: { 
-		browser: true,
-		es2021: true,
-	},
-  extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:storybook/recommended'
-	],
-  ignorePatterns: [
-		'dist',
-		'.eslintrc.cjs'
-	],
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react-refresh',
-		'react',
-	],
+  plugins: ['react-refresh'],
   rules: {
 		"@typescript-eslint/no-explicit-any": "off",
     'react-refresh/only-export-components': [
