2577d8cad2337c7aa823b94942a9385ae95e87c8
owestbury <owestbury@gmail.com>
2023-09-13 14:55:00 +0100

    fix: dropdown link text black

--- src/components/atoms/tooltip/index.tsx
+++ src/components/atoms/tooltip/index.tsx
@@ -18,7 +18,7 @@ const ToolTip: FC<ToolTipProps> = ({
 			</div>
 			<button onClick={() => {
 				setCloseToolTip(true);
-			}} type='button' className='absolute top-4 right-0 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+			}} type='button' className='absolute top-4 right-0 rounded-md p-2 inline-flex items-center justify-center text-black hover:text-gray-500  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
 				<span className='sr-only'>Close menu</span>
 
 				<svg className='h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
