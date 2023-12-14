da3412c7714625b9a0d19946d54093dfc14e3b38
owestbury <owestbury@gmail.com>
2023-12-14 22:37:03 +0000

    fixed: new basic modal compontent

---
+++ src/components/atoms/modal/index.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BasicModal} from '.';
+import type {Story} from '@storybook/react';
+
+export default {title: 'DK/Atom/BasicModal', component: BasicModal};
+
+const Template: Story<React.ComponentProps<typeof BasicModal>> = args => <BasicModal {...args} />;
+
+export const displayMessage = Template.bind({});
+displayMessage.args = {
+	repo: 'efe',
+	buttonColour: 'blue',
+	language: 'English',
+	buttonTitle: 'Everyday English',
+	displayTitle: 'Everyday English Langauge',
+	displayMessage: 'Please select your language from the list below.',
+	buttons: [
+		{repo: 'efe', href: '/en/audio/every-day-english-uk', label: 'British English', backgroundColor: 'green'},
+		{repo: 'efe', href: '/en/audio/every-day-english-us', label: 'American English', backgroundColor: 'red'},
+	],
+};
+
+displayMessage.parameters = {design: {type: 'figma', url: 'https://www.figma.com/file/5Luoo8FHwMRsF3tJhGyHsA/EFE?node-id=0%3A6843'}};
---
+++ src/components/atoms/modal/index.tsx
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import type {FC, ReactNode} from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {dklBtn} from '../../../utility/themes/dkl/theme';
+import {efeBtn, fontColor} from '../../../utility/themes/efe/theme';
+
+type ButtonProps = {
+	repo?: string;
+	/**
+	 * What background color to use
+	 */
+	backgroundColor?: string;
+	/**
+	 * Button title
+	 */
+	label: string;
+	/**
+	 * Path link
+	 */
+	href: string;
+};
+
+type BasicModalProps = {
+	repo?: string;
+	fontColorText?: string;
+	language?: string;
+	buttonColour?: string;
+	buttonTitle?: string;
+	displayTitle?: ReactNode;
+	displayMessage?: ReactNode;
+	buttons?: ButtonProps[];
+};
+
+const BasicModal: FC<BasicModalProps> = ({repo, language, buttonColour, buttonTitle, fontColorText, displayTitle, displayMessage, buttons}) => {
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const type = 'button';
+	// Repo Button Type
+	const buttonRepo = (repo: string, type: string, buttonColour: string) => {
+		switch (repo) {
+			case 'dkl':
+				return `${dklBtn[type]} ${dklBtn[buttonColour]} `;
+			case 'efe':
+				return `${efeBtn[type]} ${efeBtn[buttonColour]}`;
+			default:
+				return 'py-3 px-6 mx-auto text-center';
+		}
+	};
+
+	let buttonClass = null;
+	let pickFontColor = null;
+	if (buttonColour) {
+		pickFontColor = fontColorText ? fontColorText : 'white';
+		buttonClass = `${fontColor[pickFontColor]}`;
+	}
+
+	const backButtonStyle = buttonRepo(repo ?? '', type ?? '', buttonColour ?? '');
+
+	return (
+		<div>
+			<Button onClick={handleOpen} className='w-full'>
+				<div className={`${backButtonStyle} w-full uppercase cursor-pointer rounded text-18`}>
+					<span className='items-center justify-center w-full block rounded-lg'>
+						<span className='text-white text-18'>{language}</span>
+						<span className=' w-full block leading-7'>
+							<span className='text-white inline-block align-top p-0 font-bold tracking-[.6px]'></span>
+							<span className='text-white font-bold inline-block align-top p-0 tracking-[.6px] text-18'>{buttonTitle}</span>
+						</span>
+					</span>
+				</div>
+			</Button>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby='modal-modal-title'
+				aria-describedby='modal-modal-description'
+			>
+				<Box sx={{
+					boxShadow: 24,
+					position: 'absolute',
+					border: '1px solid #000',
+					p: 4,
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+					width: 400,
+					bgcolor: 'background.paper',
+				}}>
+					<Typography id='modal-modal-title' variant='h6' component='h2'>
+						{displayTitle}
+					</Typography>
+					<Typography id='modal-modal-description' sx={{mt: 2}}>
+						{displayMessage}
+					</Typography>
+					{buttons && (
+						<Typography id='modal-modal-description' sx={{mt: 2}}>
+							{buttons.map((button, i) => {
+								const background = buttonRepo(repo ?? '', type ?? '', button.backgroundColor ?? 'blue');
+								return (
+									<div key={i} className={`p-5 m-5 ${backButtonStyle} ${background}`}>
+										<Button sx={{color: 'white'}} href={button.href}>{button.label}</Button>
+									</div>
+								);
+							},
+							)}
+						</Typography>
+					)}
+				</Box>
+			</Modal>
+		</div>
+	);
+};
+
+export {BasicModal};
